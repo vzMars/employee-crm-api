8b512dbf5d772863580d36efb0480a78f618ad07
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-13 19:22:58 -0400

    Status Enum created which is used in the Employee model

---
+++ src/main/java/dev/marcosgonzalez/employeecrmapi/model/Employee.java
@@ -0,0 +1,70 @@
+package dev.marcosgonzalez.employeecrmapi.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+@Document(collation = "employees")
+public class Employee {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String jobTitle;
+    private Status status;
+
+    @DocumentReference
+    private User userId;
+
+    public Employee(String name, String address, String city, String state, String zipcode, String jobTitle, Status status, User userId) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.jobTitle = jobTitle;
+        this.status = status;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public String getZipcode() {
+        return this.zipcode;
+    }
+
+    public String getJobTitle() {
+        return this.jobTitle;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+}
---
+++ src/main/java/dev/marcosgonzalez/employeecrmapi/model/Status.java
@@ -0,0 +1,7 @@
+package dev.marcosgonzalez.employeecrmapi.model;
+
+public enum Status {
+    ACTIVE,
+    INACTIVE,
+
+}
