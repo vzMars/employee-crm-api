76b95d358296e8ce31d669ead182485c764afbce
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-13 11:31:18 -0400

    injected UserService into SecurityConfig and also added BCryptPasswordEncoder and AuthenticationManager beans to the config

--- src/main/java/dev/marcosgonzalez/employeecrmapi/config/SecurityConfig.java
+++ src/main/java/dev/marcosgonzalez/employeecrmapi/config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package dev.marcosgonzalez.employeecrmapi.config;
 
+import dev.marcosgonzalez.employeecrmapi.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,4 +32,13 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
