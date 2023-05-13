38dca287128af05281a566bfcab40522b8dce5c4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-13 09:37:35 -0400

    init api with connection to mongodb working

---
+++ src/test/java/dev/marcosgonzalez/employeecrmapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcosgonzalez.employeecrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
