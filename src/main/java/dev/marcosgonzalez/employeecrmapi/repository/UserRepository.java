7704fa6f5d5f0074866c476969698bb64aec6050
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-13 10:37:16 -0400

    UserRepository created which extends MongoRepository with two helper methods

---
+++ src/main/java/dev/marcosgonzalez/employeecrmapi/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package dev.marcosgonzalez.employeecrmapi.repository;
+
+import dev.marcosgonzalez.employeecrmapi.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    @Query(collation = "{'locale':  'en', 'strength':  2}")
+    Optional<User> findByUsername(String username);
+
+    @Query(collation = "{'locale':  'en', 'strength':  2}")
+    boolean existsByUsernameOrEmail(String username, String email);
+}
